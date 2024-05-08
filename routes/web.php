d4ed07f0217dcb518c7852aa08b3deb171d66750
Tolotra David <tolotradavid2@gmail.com>
2024-05-08 16:55:37 +0300

    21. All About Routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/xxx', function () {
+    return 'Hello';
+})->name('hello');
+
+Route::get('/hallo', function () {
+    return redirect()->route('hello');
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return 'Hello ' . $name;
+});
+
+Route::fallback(function () {
+    return 'Still got somewhere!';
+});
