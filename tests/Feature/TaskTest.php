e49137a752d3b1ea7a4ced796dbd56298e938c42
Tolotra David <tolotradavid2@gmail.com>
2024-05-17 17:37:56 +0300

    29. Model and Reading Data

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -4,15 +4,14 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\Task;
+use Illuminate\Support\Facades\View;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TaskTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
-    /**
-     * A basic feature test example.
-     */
+
     public function test_it_can_create_task(): void
     {
         $faker = $this->faker();
@@ -37,16 +36,54 @@ class TaskTest extends TestCase
 
     public function test_it_can_create_10_tasks(): void
     {
-        for ($i = 0; $i < 10; $i++) {
-            $faker = $this->faker();
-            Task::create([
-                "title" => $faker->name,
-                "description" => $faker->sentence(2),
-                "long_description" => $faker->sentence(5),
-                "completed" => true
-            ]);
-        }
+        Task::factory(10)->create();
 
         $this->assertEquals(10, Task::all()->count());
     }
+
+    public function test_it_can_show_all_tasks(): void
+    {
+        $tasks = Task::factory(10)->create([
+            "title" => $this->faker->name(2),
+        ]);
+
+        $view = View::make('index', ['tasks' => $tasks]);
+
+        $content = $view->render();
+
+        foreach ($tasks as $task) {
+            $this->assertStringContainsString($task->title, $content);
+        }
+    }
+
+    public function test_it_can_show_all_tasks_completed(): void
+    {
+        $tasks = Task::factory(10)->create([
+            "title" => $this->faker->name(2),
+        ]);
+
+        $view = View::make('index', ['tasks' => $tasks->where('completed', true)]);
+
+        $content = $view->render();
+
+        foreach ($tasks as $task) {
+            $this->assertStringContainsString($task->title, $content);
+        }
+    }
+
+    public function test_it_can_show_one_single_task(): void
+    {
+        $task = Task::factory()->create([
+            'title' => 'Laravel',
+            'description' => 'Laravel 10'
+        ]);
+
+        $view = View::make('show', ['task' => $task]);
+
+        $content = $view->render();
+
+        $this->assertStringContainsString($task->title, $content);
+        $this->assertStringContainsString($task->description, $content);
+    }
+
 }
